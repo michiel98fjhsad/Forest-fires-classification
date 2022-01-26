57421e1019bf37b2c335bb7b6c49334c7000b18c
michiel98fjhsad <94781488+michiel98fjhsad@users.noreply.github.com>
2022-01-26 11:38:45 +0100

    converted character columns to numeric

--- Forest firest classification.R
+++ Forest firest classification.R
@@ -1,11 +1,16 @@
 rm(list=ls())
 library(randomForest)
 library(ggplot2)
+library(dplyr)
 
 fire <- data.frame(read.csv("C:/Users/michi/Downloads/Algerian_forest_fires_dataset_UPDATE.csv"))  
+fire <- fire[1:122,]
 fire$Classes <- as.factor(fire$Classes)
-#ggplot(data = fire, mapping = aes(x= Temperature, y= Classes)) + geom_point()
-fire$Classes
-rf.model <- randomForest(Classes~., data = fire,
+i <- c(4:13)
+fire[, i]<- apply(fire[, i], 2, 
+                  function(x) as.numeric(as.character(x)))
+
+rf.model <- randomForest(Classes~., data = fire, 
                          importance = TRUE,
-                         proximity = TRUE)
+                         proximity = TRUE,)
+print(rf.model)
