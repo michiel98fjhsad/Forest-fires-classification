5ad711a5c97f66c44c272933e9e31a35594ec234
michiel98fjhsad <94781488+michiel98fjhsad@users.noreply.github.com>
2022-01-26 11:50:11 +0100

    commit

--- Forest firest classification.R
+++ Forest firest classification.R
@@ -1,7 +1,5 @@
 rm(list=ls())
 library(randomForest)
-library(ggplot2)
-library(dplyr)
 
 fire <- data.frame(read.csv("C:/Users/michi/Downloads/Algerian_forest_fires_dataset_UPDATE.csv"))  
 fire <- fire[1:122,]
@@ -14,3 +12,6 @@ rf.model <- randomForest(Classes~., data = fire,
                          importance = TRUE,
                          proximity = TRUE,)
 print(rf.model)
+head(rf.model$err.rate)
+getTree(rf.model, k=1, labelVar = FALSE)
+plot.randomForest(rf.model, type="l")
